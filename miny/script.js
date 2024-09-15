26d11bc5d712c5348860980669f463878f15c134
2.NejlepsiOstravskyRapper <151023690+macekq@users.noreply.github.com>
2024-09-15 12:17:31 +0200

    finised

--- miny/script.js
+++ miny/script.js
@@ -1,12 +1,5 @@
-var FIELD = {
-    size: 5,
+var FIELD = {size: 5}
 
-    mines:{
-        loc:[],
-        amount: 1
-    }
-
-}
 function proKtty(){
 
     window.alert('na co si hrajes\npadej z moji stranky')
@@ -14,18 +7,27 @@ function proKtty(){
 }
 function createField(){
 
-    if(parseInt(document.getElementById('fieldSize').value) ** 2 < parseInt(document.getElementById('mines').value)){
+    for(let i = 0; i<FIELD.size; i++){
+
+        try{
+            document.getElementById(`row${i}`).remove()
+        }catch(error){}    
+    }
+
+    FIELD = {
+        size: parseInt(document.getElementById('fieldSize').value),
+    
+        mines:{
+            loc:[],
+            amount: 1,
+            around: []
+        },
+        visibleCells: []
+    }
+
+    if(parseInt(document.getElementById('fieldSize').value) ** 2 -1< parseInt(document.getElementById('mines').value)){
         proKtty()
     }else{
-        for(let i = 0; i<FIELD.size; i++){
-
-            try{
-                document.getElementById(`row${i}`).remove()
-            }catch(error){}    
-        }
-
-        FIELD.size = parseInt(document.getElementById('fieldSize').value)
-        FIELD.mines.loc = []
 
         for(let i = 0; i<FIELD.size; i++){
             
@@ -37,6 +39,8 @@ function createField(){
                 let slot = document.createElement('th')
                 slot.id = `cell${i}-${j}`
 
+                slot.addEventListener('click', userAction)
+
                 row.appendChild(slot)
             }
 
@@ -45,7 +49,6 @@ function createField(){
 
         placeMines()
     }
-    console.log(FIELD)
 }
 function includesArr(mainArr, arr){
 
@@ -53,6 +56,14 @@ function includesArr(mainArr, arr){
         if(i.join('') == arr.join('')) return true
     }
     return false
+}
+function countSameValues(mainArr, arr){
+
+    let conter = 0
+    for(let i of mainArr){
+        if(i.join('') == arr.join('')) conter++
+    }
+    return conter
 }
 function placeMines(){
 
@@ -72,11 +83,128 @@ function placeMines(){
             }
         }
     }
+    
+    aroundMines()
+}
+function aroundSlot(X, Y){
+
+    return [[X+1, Y], [X-1, Y], [X+1, Y+1], [X+1, Y-1], [X-1, Y+1], [X-1, Y-1], [X, Y+1], [X, Y-1]]
+}
+function aroundMines(){
 
     for(let mine of FIELD.mines.loc){
+        let around = aroundSlot(mine[0], mine[1])
+        
+        for(let slot of around){
+            try{
+                FIELD.mines.around.push(slot)
+            }catch(error){}
+        }
+    }
+    showNums()
+}
+function showNums(){
+    for(let i = 0; i<FIELD.size; i++){
+        for(let j = 0; j<FIELD.size; j++){
 
-        document.getElementById(`cell${mine[0]}-${mine[1]}`).style.backgroundColor = 'red'
+            let color = document.getElementById('showMines').checked ? 'grey' : 'rgba(50,50,50)'
+            let slot = document.getElementById(`cell${j}-${i}`)
+            slot.style.color = color
+            if(includesArr(FIELD.mines.around, [j, i])){
+                
+                slot.style.backgroundColor = color
+                slot.innerText = `${countSameValues(FIELD.mines.around, [j, i])}`
+            }
+            if(includesArr(FIELD.mines.loc, [j, i])){
+
+                slot.style.backgroundColor = color == 'grey' ? 'red' : color
+                slot.innerText = 'x'
+            
+            }else if(!includesArr(FIELD.mines.around, [j, i])){
+
+                slot.style.background = color
+                slot.style.color = color
+                slot.innerText = '0'
+            }
+        }
     }
 }
-
 createField()
+
+function userAction(){
+    let X = parseInt(this.id.slice(4, this.id.length))
+    let Y = parseInt(this.id.slice(5 + `${X}`.length, this.id.length))
+
+    this.style.backgroundColor = 'grey'
+    
+    switch(this.innerText){
+      
+        case '0':
+            this.style.color = 'grey'
+            FIELD.visibleCells.push([X, Y])
+            showCells(X, Y)
+            break
+
+        case '1':
+            this.style.color = 'blue'
+            FIELD.visibleCells.push([X, Y])
+            break
+
+        case '2':
+            this.style.color = 'green'
+            FIELD.visibleCells.push([X, Y])
+            break
+        
+        case '3':
+            this.style.color = 'red'
+            FIELD.visibleCells.push([X, Y])
+            break
+
+        case '4':
+            this.style.color = 'purple'
+            FIELD.visibleCells.push([X, Y])
+            break
+
+        case 'x':
+            
+            console.log(FIELD.visibleCells.length)
+            if(FIELD.visibleCells.length == 0){
+                createField()
+            }else{
+                window.alert('game over\nplay again?')
+                createField()
+            }
+            break
+        
+        default:
+            this.style.color = 'black'
+            FIELD.visibleCells.push([X, Y])
+            break
+    }
+
+    if(FIELD.size **2 == FIELD.visibleCells.length + FIELD.mines.amount){
+        setTimeout(() => {
+            window.alert('you have won\nplay again?')
+            createField()
+        }, 1000)
+    }
+}
+function showCells(X, Y){
+    for(let i of aroundSlot(X, Y)){
+        
+        try{
+            
+            cell = document.getElementById(`cell${i[0]}-${i[1]}`)
+
+            if(cell.innerText == '0' && !includesArr(FIELD.visibleCells, [i[0], i[1]])){
+
+                cell.style.backgroundColor = 'grey'
+                cell.style.color = 'grey'
+
+                FIELD.visibleCells.push([i[0], i[1]])
+                showCells(i[0], i[1])
+            }
+
+        }catch(error){}
+    }
+}
