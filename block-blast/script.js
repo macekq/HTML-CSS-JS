446e67b2a314e93673dbd3e5d5689b3bb97d2c10
2.NejlepsiOstravskyRapper <151023690+macekq@users.noreply.github.com>
2024-09-21 11:55:49 +0200

    a

--- block-blast/script.js
+++ block-blast/script.js
@@ -50,19 +50,16 @@ function drawPlayingField(){
 function displayShape(X, Y, size, color, matrix){
 
     ctx.lineWidth = 3
-
+    ctx.fillStyle = color
+             
     for(let y = 0; y<5; y++){
         for(let x = 0; x<5; x++){
 
             if(matrix[y][x] == 1){
 
-                ctx.fillStyle = color
                 ctx.fillRect(X + x*size, Y + y*size, size, size)
                 ctx.strokeRect(X + x*size, Y + y*size, size, size)
         
-            }else{
-                ctx.fillStyle = 'rgba(0,0,0,0.4)'
-                ctx.fillRect(X + x*size, Y + y*size, size, size)
             }
         }
     }
@@ -91,7 +88,7 @@ function createShapeMenu(){
         ShapeMenu[i].matrix = shapes[shapes.all[Math.floor(Math.random()*shapes.all.length)]]
         ShapeMenu[i].ready = true
         ShapeMenu[i].mouse = false
-    }
+    }  
     
 
 }
@@ -99,6 +96,81 @@ function displayShapeMenu(){
     for(let i = 1; i<=3; i++){
         if(ShapeMenu[i].ready) displayShape(ShapeMenu[i].x, ShapeMenu[i].y, ShapeMenu.size, ShapeMenu[i].color, ShapeMenu[i].matrix)
     }
+}
+function displayMatrix(){
+    for(let i = 0; i<8; i++){
+
+        for(let j = 0; j<8; j++){
+
+            if(MATRIX[i][j] != 0){
+                ctx.fillStyle = MATRIX[i][j]
+                ctx.fillRect(FIELD.x + SHAPE.size*j, FIELD.y + SHAPE.size*i, SHAPE.size, SHAPE.size)
+                ctx.strokeRect(FIELD.x + SHAPE.size*j, FIELD.y + SHAPE.size*i, SHAPE.size, SHAPE.size)
+            }
+        }
+    }
+}
+function solidInside(rX, rY){
+
+    for(let y = 0; y<5; y++){
+        for(let x = 0; x<5; x++){
+
+            if(SHAPE.matrix[y][x] == 1)
+                try{
+                    
+                    if(MATRIX[rY + y][rX + x] == undefined || MATRIX[y + rY][x + rX] != 0) return false
+
+                }catch(error){
+                    
+                    return false
+                }
+            
+        }
+    }
+    
+    return true
+    
+}
+function snapping(){
+
+    let rX = Math.floor((SHAPE.x - FIELD.x)/SHAPE.size), rY = Math.floor((SHAPE.y - FIELD.y)/SHAPE.size)
+    
+    if(solidInside(rX, rY)){
+        ctx.fillStyle = 'rgba(0,0,0,0.3)'
+        for(let y = 0; y<5; y++){
+            for(let x = 0; x<5; x++){
+
+                if(SHAPE.matrix[y][x] != 0) ctx.fillRect(FIELD.x + (rX + x)* SHAPE.size, FIELD.y + (rY + y)* SHAPE.size, SHAPE.size, SHAPE.size)
+            }
+        }
+
+        SHAPE.ready = true
+    }else{
+        SHAPE.ready = false
+    }
+}
+function updateAll(){
+    ctx.clearRect(0,0, window.innerWidth, window.innerHeight)
+
+    drawPlayingField()
+    displayMatrix()
+    snapping()
+
+    if(SHAPE.user)displayShape(SHAPE.x, SHAPE.y, SHAPE.size, SHAPE.color, SHAPE.matrix)
+    displayShapeMenu()
+}
+function clearSpace(){
+
+    for(let i = 0; i<8; i++){
+        if(MATRIX[i].includes(0) == false){
+            MATRIX[i] = [0,0,0,0,0,0,0,0]
+        }
+        
+        if([MATRIX[0][i],MATRIX[1][i],MATRIX[2][i],MATRIX[3][i],MATRIX[4][i],MATRIX[5][i],MATRIX[6][i],MATRIX[7][i]].includes(0) == false){
+            MATRIX[0][i]=0,MATRIX[1][i]=0,MATRIX[2][i]=0,MATRIX[3][i]=0,MATRIX[4][i]=0,MATRIX[5][i]=0,MATRIX[6][i]=0,MATRIX[7][i]=0
+        }
+    }
+
 }
 window.addEventListener('mousedown', e => {
 
@@ -110,6 +182,7 @@ window.addEventListener('mousedown', e => {
             SHAPE.color = ShapeMenu[i].color
             SHAPE.matrix = ShapeMenu[i].matrix
             
+            SHAPE.ready = false
             SHAPE.addedX = (e.clientX - SHAPE.x)*4
             SHAPE.addedY = (e.clientY - SHAPE.y)*4
             SHAPE.user = true
@@ -123,17 +196,41 @@ window.addEventListener('mousedown', e => {
 })
 window.addEventListener('mouseup', () => {
 
-    if(SHAPE.user){
+    if(SHAPE.user && SHAPE.ready){
+
+        SHAPE.ready = false
+        SHAPE.user = false
+        ShapeMenu[SHAPE.id].ready = false
+
+        let rX = Math.floor((SHAPE.x - FIELD.x)/SHAPE.size), rY = Math.floor((SHAPE.y - FIELD.y)/SHAPE.size)
+
+        for(let y = 0; y<5; y++){
+            for(let x = 0; x<5; x++){
+
+                try{
+                    if(SHAPE.matrix[y][x] == 1){
+                        MATRIX[rY + y][rX + x] = SHAPE.color
+                    }
+                }catch(error){}
+            }
+        }
+
+        updateAll()
+    }else if(SHAPE.user){
+        
+        SHAPE.ready = false
         SHAPE.user = false
         ShapeMenu[SHAPE.id].ready = true
     
         ctx.clearRect(0,0, window.innerWidth, window.innerHeight)
         drawPlayingField()
+        displayMatrix()
         displayShapeMenu()
-    
-        console.log(ShapeMenu)
     }
-    
+
+    if(ShapeMenu[1].ready == false && ShapeMenu[2].ready == false && ShapeMenu[3].ready == false) createShapeMenu()
+
+    clearSpace()
 })
 window.addEventListener('mousemove', e => {
 
@@ -155,25 +252,34 @@ window.addEventListener('mousemove', e => {
 
     if(SHAPE.user){
 
+        SHAPE.x = e.clientX - SHAPE.addedX, SHAPE.y = e.clientY - SHAPE.addedY
+
         ctx.clearRect(0,0, window.innerWidth, window.innerHeight)
 
         drawPlayingField()
-        displayShape(e.clientX - SHAPE.addedX, e.clientY - SHAPE.addedY, SHAPE.size, SHAPE.color, SHAPE.matrix)
+        displayMatrix()
+        snapping()
+    
+        displayShape(SHAPE.x, SHAPE.y, SHAPE.size, SHAPE.color, SHAPE.matrix)
         displayShapeMenu()
+    
     }else{
         ctx.clearRect(0,0, window.innerWidth, window.innerHeight)
 
         drawPlayingField()
+        displayMatrix()
         displayShapeMenu()
     }
-    
-
 })
 
 resizeCanvas()
-window.addEventListener('resize', resizeCanvas)
+window.addEventListener('resize', () => {
+
+    resizeCanvas()
+    createShapeMenu()
+})
 
 drawPlayingField()
+displayMatrix()
 createShapeMenu()
 displayShapeMenu()
-console.log(ShapeMenu)
