311809076a0556c30ef4bb7e82270b3f4153ca6a
2.NejlepsiOstravskyRapper <151023690+macekq@users.noreply.github.com>
2024-09-24 16:35:52 +0200

    Update js.js

--- js.js
+++ js.js
@@ -2,7 +2,7 @@ window.addEventListener('touchend', e => {
 
     e.preventDefault()
 
-    let touch = e.touches[0]
+    let touch = e.changedTouches[0]
 
     document.getElementById('x').innerText = `${touch.clientX}`
     document.getElementById('y').innerText = `${touch.clientY}`
